53b87f4839349eb43302fd8f20f71d224974111a
koob-marketplace <contact@koob.sh>
2025-11-26 15:37:12 +0200

    added google analytics

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import { withPayload } from '@payloadcms/next/withPayload';
-import NextConfig from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
