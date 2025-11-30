02cbaf735cf526b74fdd9c8518014ff8d8569e98
koob-marketplace <contact@koob.sh>
2025-11-30 13:06:03 +0200

    remove footer link for blog

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -14,7 +14,6 @@ const Footer = () => {
           <Link href='/studio'>Studio</Link>
           <Link href='/converter'>Converters</Link>
           <Link href='/mcp'>MCP</Link>
-          <Link href='/blog'>Blog</Link>
           <ThemeToggle />
         </div>
 
